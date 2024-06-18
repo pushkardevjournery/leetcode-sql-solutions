86a7e3e2b9cac962019cd38c45736a433d678fa1
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2024-06-18 17:29:08 +0530

    Problem: 595 Big Countries

---
+++ problem0501To0600/595_Big_Countries.sql
@@ -0,0 +1,23 @@
+-- Table: World
+--
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | name        | varchar |
+-- | continent   | varchar |
+-- | area        | int     |
+-- | population  | int     |
+-- | gdp         | bigint  |
+-- +-------------+---------+
+-- name is the primary key (column with unique values) for this table.
+-- Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+
+SELECT
+    name,
+    population,
+    area
+FROM
+    World
+WHERE
+    area >= 3000000
+   OR population >= 25000000;
