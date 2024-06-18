9cd29e218ae09d6793f78acd39dbce84f2948f26
pushkarchauhan91 <pushkarchauhan91@pushkarchauhan91s-MacBook-Air.local>
2024-06-18 17:54:04 +0530

    Solution: 1683 Invalid Tweets

---
+++ problem1601To1700/1683_Invalid_Tweets.sql
@@ -0,0 +1,17 @@
+-- Table: Tweets
+--
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | tweet_id       | int     |
+-- | content        | varchar |
+-- +----------------+---------+
+-- tweet_id is the primary key (column with unique values) for this table.
+-- This table contains all the tweets in a social media app.
+
+SELECT
+    tweet_id
+FROM
+    Tweets
+WHERE
+    LENGTH(content) > 15;
